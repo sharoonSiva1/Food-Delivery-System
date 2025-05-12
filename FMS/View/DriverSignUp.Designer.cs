8ebda5053a6eee43575e847de92f76f3712a972f
sharoonSiva1 <sharoonsivaneswaran@gmail.com>
2025-05-12 17:29:03 +0530

    Restaurant UI Completed

--- FMS/View/DriverSignUp.Designer.cs
+++ FMS/View/DriverSignUp.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.GoBackButton = new System.Windows.Forms.Button();
+            this.ConfirmPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(502, 209);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(99, 34);
+            this.RegisterButton.TabIndex = 25;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // GoBackButton
+            // 
+            this.GoBackButton.Location = new System.Drawing.Point(502, 261);
+            this.GoBackButton.Name = "GoBackButton";
+            this.GoBackButton.Size = new System.Drawing.Size(99, 34);
+            this.GoBackButton.TabIndex = 24;
+            this.GoBackButton.Text = "Go Back";
+            this.GoBackButton.UseVisualStyleBackColor = true;
+            // 
+            // ConfirmPasswordTextBox
+            // 
+            this.ConfirmPasswordTextBox.Location = new System.Drawing.Point(328, 289);
+            this.ConfirmPasswordTextBox.Name = "ConfirmPasswordTextBox";
+            this.ConfirmPasswordTextBox.Size = new System.Drawing.Size(134, 22);
+            this.ConfirmPasswordTextBox.TabIndex = 23;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(328, 258);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(134, 22);
+            this.PasswordTextBox.TabIndex = 22;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(328, 224);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(134, 22);
+            this.UsernameTextBox.TabIndex = 21;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(328, 188);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(134, 22);
+            this.NameTextBox.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(178, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Confirm Password : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(226, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Password : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(223, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Username : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Name : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(353, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 31);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Delivery Driver";
+            // 
+            // DriverSignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.GoBackButton);
+            this.Controls.Add(this.ConfirmPasswordTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DriverSignUp";
             this.Text = "DriverSignUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Button GoBackButton;
+        private System.Windows.Forms.TextBox ConfirmPasswordTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
---
+++ FMS/View/RestaurantUI.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FMS.View
+{
+    partial class RestaurantUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ManageProfileButton = new System.Windows.Forms.Button();
+            this.ManageMenuButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(302, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Restaurant";
+            // 
+            // ManageProfileButton
+            // 
+            this.ManageProfileButton.Location = new System.Drawing.Point(309, 107);
+            this.ManageProfileButton.Name = "ManageProfileButton";
+            this.ManageProfileButton.Size = new System.Drawing.Size(176, 41);
+            this.ManageProfileButton.TabIndex = 1;
+            this.ManageProfileButton.Text = "Manage Profile";
+            this.ManageProfileButton.UseVisualStyleBackColor = true;
+            // 
+            // ManageMenuButton
+            // 
+            this.ManageMenuButton.Location = new System.Drawing.Point(309, 169);
+            this.ManageMenuButton.Name = "ManageMenuButton";
+            this.ManageMenuButton.Size = new System.Drawing.Size(176, 41);
+            this.ManageMenuButton.TabIndex = 2;
+            this.ManageMenuButton.Text = "Manage Menu";
+            this.ManageMenuButton.UseVisualStyleBackColor = true;
+            // 
+            // RestaurantUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ManageMenuButton);
+            this.Controls.Add(this.ManageProfileButton);
+            this.Controls.Add(this.label1);
+            this.Name = "RestaurantUI";
+            this.Text = "RestaurantUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ManageProfileButton;
+        private System.Windows.Forms.Button ManageMenuButton;
+    }
+}
