0fcf59ad0674df069e98b3a67f87607b99a77f56
sharoonSiva1 <sharoonsivaneswaran@gmail.com>
2025-05-12 16:40:47 +0530

    Fix

--- FMS/View/CustomerSignUp.Designer.cs
+++ FMS/View/CustomerSignUp.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(399, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer";
+            // 
+            // CustomerSignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerSignUp";
             this.Text = "CustomerSignUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
