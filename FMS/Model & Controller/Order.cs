65ea1a7e3793b761912042b538c14261de3b251a
silentmist2 <methmikanever@gmail.com>
2025-05-13 12:28:03 +0530

    Added Order Model

---
+++ FMS/Model & Controller/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Model___Controller
+{
+    internal class Order
+    {
+        public int OrderID { get; set; }            // Primary key
+        public int CustomerID { get; set; }         // Foreign key
+        public string Items { get; set; }           // List of items in string format
+        public string Quantity { get; set; }        // Quantity for each item (or combined info)
+        public string Status { get; set; }          // Pending / Delivered / Cancelled etc.
+        public decimal TotalCost { get; set; }      // Order total
+        public string DeliveryAddress { get; set; } // Customer-entered address
+        public string PaymentMethod { get; set; }
+    }
+}
